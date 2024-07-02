bcb92d1f9563fe7d654616e8bfb8126c35ce1dc2
Krishna <krishna.21uad@mahendra.info>
2024-07-02 15:30:44 +0530

    Created the Spring Starter

---
+++ src/main/java/org/springdemo/spring_boot_web/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.springdemo.spring_boot_web.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Welcome to The Web Word";
+    }
+
+}
