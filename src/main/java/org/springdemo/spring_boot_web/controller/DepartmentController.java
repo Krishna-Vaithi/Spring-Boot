8eaa9b9218deeed13a8af860343057e16f44f971
Krishna <krishna.21uad@mahendra.info>
2024-07-02 17:46:02 +0530

    Department Save API

--- src/main/java/org/springdemo/spring_boot_web/controller/DepartmentController.java
+++ src/main/java/org/springdemo/spring_boot_web/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import org.springdemo.spring_boot_web.entity.Department;
 import org.springdemo.spring_boot_web.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,8 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
-    @PostMapping("/department")
-    public Department saveDepartment(Department department){
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
     }
 }
